65d4be764e1a64e259851b80cd8360318efe261c
Nick Whitt <nick.whitt@protonmail.com>
2020-04-27 13:01:47 -0400

    family nodes/links

--- NobleTitles.py
+++ NobleTitles.py
@@ -1,6 +1,6 @@
 from gramps.gen.lib import Person
-from gramps.gen.plug.report import (MenuReportOptions, Report, stdoptions)
-from gramps.gen.plug.menu import (PersonOption, PersonListOption, ColorOption)
+from gramps.gen.plug.report import (MenuReportOptions, Report, stdoptions, utils)
+from gramps.gen.plug.menu import (PersonOption, PersonListOption, BooleanOption, ColorOption)
 from gramps.gen.lib import (EventType, AttributeType)
 from gramps.gen.utils.db import (get_birth_or_fallback, get_death_or_fallback)
 from gramps.gen.display.place import displayer as place_displayer
@@ -16,9 +16,16 @@ class NobleTitlesOptions(MenuReportOptions):
 
     def add_menu_options(self, menu):
         person_list = PersonListOption('People of interest')
-        person_list.set_help('People of interest are used as a starting '
-                               'point when determining "family lines".')
         menu.add_option('People of Interest', 'gidlist', person_list)
+
+        withparents = BooleanOption(('Include ancestors'), False)
+        menu.add_option('People of Interest', 'withparents', withparents)
+
+        withchildren = BooleanOption(('Include descendents'), False)
+        menu.add_option('People of Interest', 'withchildren', withchildren)
+
+        withspouses = BooleanOption(('Include descendent spouses'), False)
+        menu.add_option('People of Interest', 'withspouses', withspouses)
 
         color_males = ColorOption('Males', '#e0e0ff')
         color_males.set_help('The color to use to display men')
@@ -47,12 +54,10 @@ class NobleTitles(Report):
 
         self.database = database
 
-        self._families = set()
-        self._people = set()
-        for gid in options.menu.get_option_by_name('gidlist').get_value().split():
-            person = self.database.get_person_from_gramps_id(gid)
-            if person:
-                self._people.add(person.get_handle())
+        self._gidlist = options.menu.get_option_by_name('gidlist').get_value().split()
+        self._withparents = options.menu.get_option_by_name('withparents').get_value()
+        self._withchildren = options.menu.get_option_by_name('withchildren').get_value()
+        self._withspouses = options.menu.get_option_by_name('withspouses').get_value()
 
         self.fillcolor = {
             Person.MALE:    options.menu.get_option_by_name('colormales').get_value(),
@@ -67,20 +72,105 @@ class NobleTitles(Report):
         }
 
     def begin_report(self):
-        poi = set(self._people)
-        for handle in poi:
-            person = self.database.get_person_from_handle(handle)
+        self._people = set()
+        self._families = set()
 
-            for family_handle in person.get_family_handle_list():
-                self._families.add(family_handle)
+        for gid in self._gidlist:
+            person = self.database.get_person_from_gramps_id(gid)
 
-                family = self.database.get_family_from_handle(family_handle)
-                for childref in family.get_child_ref_list():
-                    child = self.database.get_person_from_handle(childref.ref)
-                    self._people.add(child.get_handle())
+            if self._withchildren:
+                self.add_children(person)
 
+            if self._withparents:
+                self.add_parents(person)
 
     def write_report(self):
+        self.write_people()
+        self.write_families()
+
+    def add_children(self, person):
+        self._people.add(person.get_handle())
+
+        for family_handle in person.get_family_handle_list():
+            self._families.add(family_handle)
+
+            family = self.database.get_family_from_handle(family_handle)
+            for childref in family.get_child_ref_list():
+                child = self.database.get_person_from_handle(childref.ref)
+                self.add_children(child)
+
+            if self._withspouses:
+                spouse_handle = utils.find_spouse(person, family)
+                if spouse_handle:
+                    self._people.add(spouse_handle)
+
+    def add_parents(self, person):
+        self._people.add(person.get_handle())
+
+        for family_handle in person.get_parent_family_handle_list():
+            self._families.add(family_handle)
+
+            family = self.database.get_family_from_handle(family_handle)
+
+            father_handle = family.get_father_handle()
+            if father_handle:
+                father = self.database.get_person_from_handle(father_handle)
+                self.add_parents(father)
+
+            mother_handle = family.get_mother_handle()
+            if mother_handle:
+                mother = self.database.get_person_from_handle(mother_handle)
+                self.add_parents(mother)
+
+    def write_families(self):
+        for handle in self._families:
+            family = self.database.get_family_from_handle(handle)
+
+            self.doc.add_node(node_id=family.get_gramps_id(),
+                              label='family',
+                              shape='ellipse',
+                              color='',
+                              style='solid,filled',
+                              fillcolor='#ffffe0',
+                              htmloutput=False)
+
+            self.doc.start_subgraph(family.get_gramps_id())
+
+            father_handle = family.get_father_handle()
+            if father_handle:
+                if father_handle in self._people:
+                    father = self.database.get_person_from_handle(father_handle)
+                    self.doc.add_link(id1=father.get_gramps_id(),
+                                      id2=family.get_gramps_id(),
+                                      style='',
+                                      head='none',
+                                      tail='none',
+                                      comment='')
+
+            mother_handle = family.get_mother_handle()
+            if mother_handle:
+                if mother_handle in self._people:
+                    mother = self.database.get_person_from_handle(mother_handle)
+                    self.doc.add_link(id1=mother.get_gramps_id(),
+                                      id2=family.get_gramps_id(),
+                                      style='',
+                                      head='none',
+                                      tail='none',
+                                      comment='')
+
+            self.doc.end_subgraph()
+
+            for childref in family.get_child_ref_list():
+                if childref.ref in self._people:
+                    child = self.database.get_person_from_handle(childref.ref)
+                    self.doc.add_link(id1=family.get_gramps_id(),
+                                      id2=child.get_gramps_id(),
+                                      style='',
+                                      head='none',
+                                      tail='none',
+                                      comment='')
+
+    def write_people(self):
         for handle in self._people:
             person = self.database.get_person_from_handle(handle)
 
